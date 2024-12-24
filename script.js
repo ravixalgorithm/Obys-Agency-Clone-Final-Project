fadf980fd85c0df1e4e132e76599fe38fd6f5e4e
Mr. Algorithm <11aravipratapsingh@gmail.com>
2024-12-24 17:08:43 +0530

    Locomotive Js Added to project

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+function locomotiveAnimation(){
+  gsap.registerPlugin(ScrollTrigger);
+
+// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+const locoScroll = new LocomotiveScroll({
+  el: document.querySelector("#main"),
+  smooth: true
+});
+// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+locoScroll.on("scroll", ScrollTrigger.update);
+
+// tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+ScrollTrigger.scrollerProxy("#main", {
+  scrollTop(value) {
+    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
+  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+  getBoundingClientRect() {
+    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
+  },
+  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+  pinType: document.querySelector("#main").style.transform ? "transform" : "fixed"
+});
+
+// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
+ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+ScrollTrigger.refresh();
+
+}
 function loadingAnimation() {
   var tl = gsap.timeline();
 
@@ -48,18 +79,16 @@ function loadingAnimation() {
     y: 100,
     stagger: 0.2,
   });
+
+  tl.from("#hero1, #page2", {
+    opacity: 0,
+  }, "-=1.2");
 }
 
 function mouseAnimation() {
-  document.addEventListener("mousemove", function (e) {
-    gsap.to("#cursor", {
-      top: e.y,
-      left: e.x,
-    });
-  });
-
   Shery.makeMagnet("#navpart2 h4", {});
 }
 
 loadingAnimation();
 mouseAnimation();
+locomotiveAnimation();
